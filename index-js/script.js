59d797ed4ce1082257838e87a34adca8fb990a60
NaufalArkaan <naufalarkaan25@gmail.com>
2023-12-07 22:26:49 +0800

    style css

--- index-js/script.js
+++ index-js/script.js
@@ -3,26 +3,34 @@ const alas = document.getElementById('alas')
 const tinggi = document.getElementById('tinggi')
 const hitung = document.getElementById('hitung')
 const output = document.getElementById('output')
-hitung.addEventListener('click', function(){
+hitung.addEventListener('click', function () {
     let a = alas.value
     let t = tinggi.value
-    let l = 0.5*a*t
-    output.innerHTML = `Hasil luas segitiga adalah ${l} cm2`
+    let l = 0.5 * a * t
+    output.innerHTML = `L = 1/2 x Alas x Tinggi <br> L = 1/2 x ${a} x ${t} <br> L = ${l} cm2`
 })
 
 // RESET LUAS
 function resetForm() {
     // Reset input form values
     document.getElementById('inputForm').reset();
-    
-    // Hide output container and display input container
-    document.getElementById('output').style.display = 'none';
+
+}
 
 
-    }
+// JS KELILING
+var kelilingA = document.getElementById('kelilingA')
+var hitungKll = document.getElementById('hitungKll')
+var cariKeliling = document.getElementById('cariKeliling')
+hitungKll.addEventListener('click', function () {
+    var o = kelilingA.value
+    var hasil = 3 * o;
+    cariKeliling.innerHTML = `Jadi Kelilingnya Adalah ${hasil} `
+})
 
-// let btnClear = document.querySelector('#reset');
-// let inputs = document.querySelectorAll('#output');
-// btnClear.addEventListener('click', () => {
-//     inputs.forEach(output =>  output.value = '');
-// });
+// RESET KELILING
+function resetFormKeliling() {
+    // Reset input form values
+    document.getElementById('inputFormKeliling').reset();
+
+}
